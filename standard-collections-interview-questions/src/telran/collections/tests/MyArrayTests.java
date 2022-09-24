3c8bbbdca316bb622316da2fec18a69f501d0f86
User <User@User-PC.Dlink>
2022-09-24 11:24:00 +0300

    HW - 29

---
+++ standard-collections-interview-questions/src/telran/collections/tests/MyArrayTests.java
@@ -0,0 +1,19 @@
+package telran.collections.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MyArrayTests {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ standard-collections-interview-questions/src/telran/util/MyArray.java
@@ -0,0 +1,44 @@
+package telran.util;
+/**
+ * All methods of the class should have complexity O[1]
+ * @author User
+ *
+ * @param <T>
+ */
+public class MyArray<T> {
+//TODO fields
+	public MyArray(int size) {
+		// TODO
+	}
+
+	/**
+	 * sets all array's elements with a given value
+	 * 
+	 * @param value
+	 */
+	public void setAll(T value) {
+//		TODO
+//		все элементам массива дает одно и тоже значение
+	}
+
+	/**
+	 * 
+	 * @param index
+	 * @return value at given index or null if index is wrong
+	 */
+	public T get(int index) {
+//		TODO
+		return null;
+	}
+
+	/**
+	 * sets a given value at a given index
+	 * throws IndexOutOfBogunsException in the case of wron index
+	 * 
+	 * @param index
+	 * @param value
+	 */
+	public void set(int index, T value) {
+//TODO
+	}
+}
