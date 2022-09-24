30a67ec96c3d103f47f7c11dfc0cb8884aa75c19
User <User@User-PC.Dlink>
2022-09-24 13:45:23 +0300

    HW - 29

--- standard-collections-interview-questions/src/telran/collections/tests/AnagramTests.java
+++ standard-collections-interview-questions/src/telran/collections/tests/AnagramTests.java
@@ -4,11 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class AnagramTests {
+import telran.util.Anagram;
 
+class AnagramTests {
 	@Test
 	void testIsAnagram() {
-//		TODO
+		String str1 = "Hello";
+		String str2 = "Helll";
+		String str3 = "eloHl";
+		String str4 = "Hel";
+		String str5 = "Abcdf";
+		assertTrue(Anagram.isAnagram(str1, str1));
+		assertTrue(Anagram.isAnagram(str1, str3));
+		assertFalse(Anagram.isAnagram(str1, str2));
+		assertFalse(Anagram.isAnagram(str1, str4));
+		assertFalse(Anagram.isAnagram(str1, str5));
 	}
-
 }
--- standard-collections-interview-questions/src/telran/util/Anagram.java
+++ standard-collections-interview-questions/src/telran/util/Anagram.java
@@ -1,16 +1,43 @@
 package telran.util;
 
+import java.util.HashMap;
+
 public class Anagram {
 	/**
 	 * 
 	 * @param word
 	 * @param anagram
 	 * @return true if anagram is one of the possible anagrams of a given word
-	 * anagram is a string containing all symbols from a fiven word with different order
-	 * Example: yellow (wolely, lowlye, yellow), wrong anagrams (yello, yelllw)
+	 *         anagram is a string containing all symbols from a fiven word with
+	 *         different order Example: yellow (wolely, lowlye, yellow), wrong
+	 *         anagrams (yello, yelllw)
 	 */
-public static boolean isAnagram(String word, String anagram) {
-//	TODO
-	return false;
-}
+	public static boolean isAnagram(String word, String anagram) {
+		if (word.length() == anagram.length() && itReallyAnagram(word, anagram)) {
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean itReallyAnagram(String word, String anagram) {
+		HashMap<Character, Integer> map = getMap(word);
+		for (int i = 0; i < anagram.length(); i++) {
+			if (map.containsKey(anagram.charAt(i)) && (map.get(anagram.charAt(i)) > 0)) {
+				map.put(anagram.charAt(i), map.get(anagram.charAt(i)) - 1);
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static HashMap<Character, Integer> getMap(String word) {
+		HashMap<Character, Integer> res = new HashMap<>();
+		for (int i = 0; i < word.length(); i++) {
+			Integer count = res.getOrDefault(word.charAt(i), 0);
+			res.put(word.charAt(i), count + 1);
+		}
+		return res;
+	}
+
 }
