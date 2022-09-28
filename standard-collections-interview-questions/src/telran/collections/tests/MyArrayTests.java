1897b07e073df1ac1ee0062d552bf0f700439809
User <User@192.168.0.132>
2022-09-28 12:47:12 +0300

    HW - 29

--- standard-collections-interview-questions/src/telran/collections/tests/MyArrayTests.java
+++ standard-collections-interview-questions/src/telran/collections/tests/MyArrayTests.java
@@ -14,31 +14,34 @@ class MyArrayTests {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		for (int i = 0; i < length; i++) {
-			intArray.set(i, i + 1);
-		}
+//		for (int i = 0; i < length; i++) {
+//			intArray.set(i, i + 1);
+//		}
+		intArray.setAll(1);
 	}
 
 	@Test
 	void getTest() {
-		assertEquals(5, intArray.get(4));
+		assertEquals(1, intArray.get(0));
 		assertEquals(null, intArray.get(-4));
 		assertEquals(null, intArray.get(14));
 	}
 
 	@Test
 	void setTest() {
+		intArray.set(1, 10);
+		assertEquals(10, intArray.get(1));
 		assertThrows(IndexOutOfBoundsException.class, () -> intArray.set(10, 4));
 		assertThrows(IndexOutOfBoundsException.class, () -> intArray.set(-10, 4));
 	}
 
 	@Test
 	void setAllTest() {
-		intArray.setAll(100);
-		assertEquals(100, intArray.get(3));
+		intArray.set(3, 7);
+		assertEquals(7, intArray.get(3));
 		intArray.setAll(0);
 		assertEquals(0, intArray.get(2));
-		intArray.set(1, 10);
-		assertEquals(10, intArray.get(1));
+		intArray.set(2, 10);
+		assertEquals(10, intArray.get(2));
 	}
 }
